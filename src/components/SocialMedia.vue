60a2ab7225363b1e7594c0a8838096389356e9d9
Kunal J. Pardeshi <kunaljp16@gmail.com>
2022-04-10 02:36:39 +0200

    social media component added

---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,21 @@
+<script>
+import TwitterWhite from "../assets/icons/TwitterWhite.vue";
+import YoutubeWhite from "../assets/icons/YoutubeWhite.vue";
+export default {
+  components: { TwitterWhite, YoutubeWhite },
+};
+</script>
+
+<template>
+  <div
+    class="col-lg-3 col-md-6 offset-lg-6 col-sm-12 text-md-end text-sm-center"
+  >
+    <span class="pe-3"> Follow us on </span>
+    <span class="pe-3"
+      ><a href="javascript:void(0)"><TwitterWhite /></a
+    ></span>
+    <span
+      ><a href="javascript:void(0)"><YoutubeWhite /></a
+    ></span>
+  </div>
+</template>
